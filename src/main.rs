cae2a824031adc68bf676f4639d3cefc2986c553
xecarlox94 <jf94.uk@gmail.com>
2025-02-13 17:13:31 +0000

    working on program

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,38 @@
-use std::env;
-use std::fs;
+use std::{
+    env, 
+    fs,
+    io,
+    path::PathBuf
+};
+
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    let mut path: PathBuf = env::current_dir()?;
+
+
+    let project_name: String = "sf".to_string();
+    let docker_base_name: String = "ubuntu".to_string();
+    let x11_support: bool = true;
+    let nvidia_runtime: bool = true;
+    let is_debian_based: bool = true;
+
+    let new_docker_project: NewDockerProject 
+        = NewDockerProject::new(
+            project_name, 
+            docker_base_name, 
+            x11_support, 
+            nvidia_runtime, 
+            is_debian_based
+        );
+
+    new_docker_project.bootstrap_docker_project(&mut path);
+
+    println!("RAN CODE!");
+
+    Ok(())
+}
+
 
 
 struct DockerOptions {
@@ -14,30 +47,47 @@ struct NewDockerProject {
     docker_options: DockerOptions,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+impl NewDockerProject {
 
-    let path = env::current_dir()?;
-    let path_str: String = path.display().to_string();
-    println!("current path: {path_str}");
+    fn new(
+        project_name: String, 
+        docker_base_name: String, 
+        x11_support: bool, 
+        nvidia_runtime: bool, 
+        is_debian_based: bool
+    ) 
+    -> NewDockerProject {
 
-    let new_docker_project: NewDockerProject = get_new_docker_project();
-
-    Ok(())
-}
-
-fn get_new_docker_project() -> NewDockerProject {
-
-    NewDockerProject {
-        name: String::from("sf"),
-        docker_base_name: String::from("ubuntu"),
-        docker_options:
-            DockerOptions {
-                x11_support: true,
-                nvidia_runtime: true,
-                is_debian_based: true,
-            }
+        NewDockerProject {
+            name: project_name,
+            docker_base_name,
+            docker_options:
+                DockerOptions {
+                    x11_support,
+                    nvidia_runtime,
+                    is_debian_based,
+                }
+        }
     }
+
+    fn bootstrap_docker_project(&self, current_dir: &mut PathBuf) -> Result<(), io::Error> {
+
+        current_dir.push("new_dir");
+
+        //let curr_path_string: String = current_dir.display().to_string();
+
+        let curr_dir_str: String = current_dir.display().to_string();
+
+        fs::create_dir(current_dir)?;
+
+        println!("CURRENT_DIR: {curr_dir_str}");
+
+        Ok(())
+    }
+
 }
+
+
 
 // Requirements docker bootstrap
 //
