f054112521e1aeac97ca925af66306ed60731557
xecarlox94 <jf94.uk@gmail.com>
2025-02-13 14:13:40 +0000

    requirements small project

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,82 @@
-fn main() {
 
-    println!("Hello World");
 
+
+
+use std::env;
+
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+
+    let path = env::current_dir()?;
+
+    let path_str: String = path.display().to_string();
+
+    println!("current path: {path_str}");
+
+    Ok(())
 }
 
 
+
+
+// Requirements docker bootstrap
+//
+// selects project name
+//
+// selects docker image
+//
+// asks if it is debian
+//
+// asks if X11 support is needed
+//
+// asks if nvidia runtime is needed
+//
+// generates initial dockerfile
+//
+// generates an executable file called run.sh
+//
+//      it generates a cmd string
+//      it generates a docker args with sane defaults
+//
+// example:
+//
+// echo -e "clear &&\\
+//     docker_build.sh &&\\
+//     docker_run.sh \\
+//         \"\\
+//             bash \\
+//         \"\\
+//         \"\\
+//             -v '\${PWD}/src':/src \\
+//             --rm \\
+//             --privileged \\
+//             --name $PROJECT_FOLDER \\
+//         \"\\
+//         $ADD_OPTS" > run.sh
+
+
+// Requirements docker build
+//
+// removes old dangling images and volumes
+//
+// gen_container_name from current directory
+//
+// runs docker run
+
+
+// Requirements docker run
+//
+// checks run arguments for Nvidia and X11
+//
+//
+//     CMD="\
+//     $XHOST \
+//     sudo docker run \
+//     -it \
+//     -v $HOME/.config/.FILES:/root/.config/.FILES \
+//     $DOCKER_ARGS \
+//     $ADD_OPTS \
+//     $DOCKER_NAME \
+//     $RUN_CMD \
+//     "
