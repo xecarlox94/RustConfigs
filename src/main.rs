5c72e62d1b52d002504d29b4c2b57fd248aff1fd
xecarlox94 <jf94.uk@gmail.com>
2025-02-23 18:27:47 +0000

    refactoring main code

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,31 @@
 use std::env;
 
+use rust_configs::NewDockerProject;
 
-fn main() -> Result<(), Box<dyn std::error::Error>>{
+fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let curr_path = env::current_dir()?;
 
-    let new_docker_project =
-        rust_configs::
-            NewDockerProject::new(
-                "sf".to_string(),
-                "ubuntu".to_string(),
-                true,
-                true,
-                true,
-            );
-
-    new_docker_project.bootstrap_docker_project(curr_path)?;
+    build_testing_docker_project().bootstrap_docker_project(curr_path)?;
 
     Ok(())
 }
 
+
+fn build_testing_docker_project() -> NewDockerProject {
+
+    let project_name = "sf".to_string();
+    let docker_base_name = "ubuntu".to_string();
+    let x11_support = true;
+    let nvidia_runtime = true;
+    let is_debian_based = true;
+
+    NewDockerProject::new(
+        project_name,
+        docker_base_name,
+        x11_support,
+        nvidia_runtime,
+        is_debian_based,
+    )
+}
 
