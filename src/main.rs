695aa6bf08ee2f59284b1b7e22680681508e505e
xecarlox94 <jf94.uk@gmail.com>
2025-02-23 18:00:48 +0000

    refactoring code

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,40 @@
-use std::env;
-use std::path::PathBuf;
-
-use rust_configs::{
-    NewDockerProject,
+use std::{
+    env,
+    path::PathBuf,
 };
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+fn main() -> Result<(), Box<dyn std::error::Error>>{
 
     let mut curr_path: PathBuf = env::current_dir()?;
 
+    let new_docker_project =
+        rust_configs::
+            NewDockerProject::new(
+                "sf".to_string(),
+                "ubuntu".to_string(),
+                true,
+                true,
+                true,
+            );
 
-    let project_name: String = "sf".to_string();
-    let docker_base_name: String = "ubuntu".to_string();
-    let x11_support: bool = true;
-    let nvidia_runtime: bool = true;
-    let is_debian_based: bool = true;
+    println!(
+        "\n\nDOCKER FILE\n\n{}",
+        new_docker_project.get_dockerfile()
+    );
 
-    let new_docker_project: NewDockerProject
-        = NewDockerProject::new(
-            project_name,
-            docker_base_name,
-            x11_support,
-            nvidia_runtime,
-            is_debian_based
-        );
+    println!(
+        "\n\nDOCKER RUN\n\n{}",
+        new_docker_project.get_docker_buildrun_file()
+    );
 
-    println!("\n\nDOCKER FILE\n\n");
-    println!("{}", new_docker_project.get_dockerfile());
+    println!(
+        "\n\nDOCKER bootstrap\n\n"
+    );
 
-    println!("\n\nDOCKER RUN\n\n");
-    println!("{}", new_docker_project.get_docker_buildrun_file());
-
-    println!("\n\nDOCKER bootstrap\n\n");
     new_docker_project.bootstrap_docker_project(&mut curr_path)?;
 
     Ok(())
 }
+
+
